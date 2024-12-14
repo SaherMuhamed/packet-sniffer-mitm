135e56de003040dbbf448cf8b84e0782111233aa
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2024-12-14 17:23:51 +0200

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,6 +6,9 @@ from argparse import ArgumentParser
 from colorama import Fore
 import datetime as dt
 import sys
+import logging
+
+logging.basicConfig(filename="Access.log", level=logging.INFO, format="%(asctime)s - %(message)s")  # configure logging
 
 if sys.version_info < (3, 0):
     sys.stderr.write("\nYou need python 3.0 or later to run this script\n")
@@ -29,29 +32,37 @@ def sniff(interface):
 
 
 def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):  # check if a packet contains any layers (http)
-        print(Fore.RESET + f"==============================================================================")
-        print(Fore.RESET + "* Method Type ==> " + str(packet[http.HTTPRequest].Method, encoding="UTF-8"))
-        print(Fore.RESET + "* Requested from ==> " + packet[scapy.IP].src)
-        # print(packet.show())
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(Fore.RESET + "* User-Agent ==> " + str(packet[http.HTTPRequest].User_Agent, encoding="UTF-8"))
-        print(Fore.RESET + "* URL ==> " + str(url, encoding="UTF-8") + " is at " + str(packet[scapy.IP].dst))
+    # check if the packet contains an HTTP request
+    if packet.haslayer(http.HTTPRequest):
+        print(
+            Fore.RESET + f'{packet[scapy.IP].src} >> {str(packet[scapy.IP].dst)} ─ [{str(dt.datetime.now().strftime("%d/%b/%Y:%H:%M:%S"))}] "{str(packet[http.HTTPRequest].Method, encoding="UTF-8")} ' 
+                         f'/{str(packet[http.HTTPRequest].Host, encoding="UTF-8") + str(packet[http.HTTPRequest].Path, encoding="UTF-8")}" {str(packet[http.HTTPRequest].User_Agent, encoding="UTF-8")}' + "\n")
 
         if packet.haslayer(scapy.Raw):  # check if a packet contains any layers (raw)
-            load = str(packet[scapy.Raw].load)
             keywords = ['uname', 'username', 'login', 'usr', 'usrname', 'pass', 'email', 'password', 'passwd']
             for keyword in keywords:
-                if keyword in load:
-                    print(Fore.YELLOW + "* Possible credentials ==> " + load)
+                if keyword in str(packet[scapy.Raw].load):
+                    print(Fore.YELLOW +
+                          f'{packet[scapy.IP].src} >> {str(packet[scapy.IP].dst)} ─ [{str(dt.datetime.now().strftime("%d/%b/%Y:%H:%M:%S"))}] "{str(packet[http.HTTPRequest].Method, encoding="UTF-8")} '
+                          f'/{str(packet[http.HTTPRequest].Host, encoding="UTF-8") + str(packet[http.HTTPRequest].Path, encoding="UTF-8")} {str(packet[scapy.Raw].load, encoding="UTF-8")}" {str(packet[http.HTTPRequest].User_Agent, encoding="UTF-8")}' + "\n")
+                    logging.critical(
+                        msg=f'{packet[scapy.IP].src} >> {str(packet[scapy.IP].dst)} ─ [{str(dt.datetime.now().strftime("%d/%b/%Y:%H:%M:%S"))}] "{str(packet[http.HTTPRequest].Method, encoding="UTF-8")} '
+                            f'/{str(packet[http.HTTPRequest].Host, encoding="UTF-8") + str(packet[http.HTTPRequest].Path, encoding="UTF-8")} {str(packet[scapy.Raw].load, encoding="UTF-8")}" {str(packet[http.HTTPRequest].User_Agent, encoding="UTF-8")}')
                     break
-        print(Fore.RESET + f"==============================================================================")
-        print("\n")
+
+    # check if the packet contains an HTTP response
+    elif packet.haslayer(http.HTTPResponse):
+        print(
+            f'{packet[scapy.IP].dst} << {str(packet[scapy.IP].src)} ─ [{str(dt.datetime.now().strftime("%d/%b/%Y:%H:%M:%S"))}]" {packet[http.HTTPResponse].Status_Code.decode() if packet[http.HTTPResponse].Status_Code else "Unknown"} {packet[http.HTTPResponse].Content_Length.decode() if packet[http.HTTPResponse].Content_Length else "Unknown"}' + "\n")
 
 
-print('\nNetwork packet sniffer, developed and coded by Saher Muhamed - version 1.0.1\n')
-print("==========================")
-print("* " + str(dt.datetime.now().strftime("%b %d, %Y %H:%M:%S %p")))
-print("* start sniffing on " + args().iface)
-print("==========================\n")
+print(f"""
+ ____            _        _   ____        _  __  __ 
+|  _ \ __ _  ___| | _____| |_/ ___| _ __ (_)/ _|/ _|
+| |_) / _` |/ __| |/ / _ \ __\___ \| '_ \| | |_| |_ 
+|  __/ (_| | (__|   <  __/ |_ ___) | | | | |  _|  _|   {args().iface} φ
+|_|   \__,_|\___|_|\_\___|\__|____/|_| |_|_|_| |_|     {str(dt.datetime.now().strftime("%b %d, %Y %I:%M %p"))}
+""")
+print('Network packet sniffer, developed and coded by Saher Muhamed - version 1.1.2')
+print("────────────────────────────────────────────────────────────────────────────\n")
 sniff(interface=args().iface)
